7902198b910252230620bc8784d61a0502322dcd
Rige02 <robert.aron.tx@verizon.net>
2016-04-16 16:44:35 -0500

    Background ETA Checker
    Background Checker

--- app/src/main/java/hackdfw2016/smartalarm/BootReceiver.java
+++ app/src/main/java/hackdfw2016/smartalarm/BootReceiver.java
@@ -1,7 +1,19 @@
 package hackdfw2016.smartalarm;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
 /**
  * Created by HeyImRige on 4/16/2016.
  */
-public class BootReceiver {
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            // Set the alarm here.
+            Toast.makeText(context, "SMART ALARM BACKGROUND RUNNING1",Toast.LENGTH_SHORT).show();
+        }
+    }
 }
