81a1ad21ae64d41d5d0f12cb2c8d64b2a3342c31
Rige02 <robert.aron.tx@verizon.net>
2016-04-16 17:30:47 -0500

    setup on start background Checker
    about to test if it's working

--- app/src/main/java/hackdfw2016/smartalarm/BackgroundETAChecker.java
+++ app/src/main/java/hackdfw2016/smartalarm/BackgroundETAChecker.java
@@ -3,5 +3,19 @@ package hackdfw2016.smartalarm;
 /**
  * Created by HeyImRige on 4/16/2016.
  */
-public class BackgroundETAChecker {
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class BackgroundETAChecker extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context arg0, Intent arg1) {
+        System.out.println("OISDFDSOIFSDF\n");
+        // For our recurring task, we'll just display a message
+        Toast.makeText(arg0, "I'm running...BackgroundETA Chekcer", Toast.LENGTH_SHORT).show();
+    }
 }
--- app/src/main/java/hackdfw2016/smartalarm/BootReceiver.java
+++ app/src/main/java/hackdfw2016/smartalarm/BootReceiver.java
@@ -1,5 +1,7 @@
 package hackdfw2016.smartalarm;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,11 +11,27 @@ import android.widget.Toast;
  * Created by HeyImRige on 4/16/2016.
  */
 public class BootReceiver extends BroadcastReceiver {
+
+    private PendingIntent pendingIntent;
+    private AlarmManager manager;
+
+
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
-            // Set the alarm here.
+            //debug stuff for starting the alarm
             Toast.makeText(context, "SMART ALARM BACKGROUND RUNNING1",Toast.LENGTH_SHORT).show();
+
+
+
+
+            //setup repeating alarm
+            Intent alarmIntent = new Intent(context,BackgroundETAChecker.class);
+            pendingIntent = PendingIntent.getBroadcast(context, 0, alarmIntent, 0);
+            manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            int interval = 10000;
+            manager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), interval, pendingIntent);
+            Toast.makeText(context, "Alarm Set", Toast.LENGTH_SHORT).show();
         }
     }
 }
