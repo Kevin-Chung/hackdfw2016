440bfb6ea53db08e1741ee79dcd35fa1d9f96180
scott nidell <snidell@scotts-MacBook-Pro.local>
2016-04-17 00:52:02 -0500

    linking activity

--- app/src/main/java/hackdfw2016/smartalarm/weatherAPI.java
+++ app/src/main/java/hackdfw2016/smartalarm/weatherAPI.java
@@ -1,24 +1,116 @@
 package hackdfw2016.smartalarm;
 
 import java.io.IOException;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+
+import android.location.Location;
+import android.widget.TextView;
 
 /**
  * Created by snidell on 4/16/16.
  */
-public class weatherAPI {
-    String APIkey="https://github.com/Kevin-Chung/hackdfw2016.git";
-    String url="";
-    public weatherAPI(){
+public class weatherAPI extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener {
+    String APIkey = "https://github.com/Kevin-Chung/hackdfw2016.git";
+    String url = "";
+    GoogleApiClient mGoogleApiClient;
+
+
+    public weatherAPI() {
+
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder().url(url).build();
 
-        //Response response = client.newCall(request).execute();
+
+        try {
+            Response response = client.newCall(request).execute();
+        } catch (IOException ex) {
+            System.out.print(ex.toString());
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_activity);
+
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
 
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
 
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // We are now connected!
+        Location mLastLocation;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            setContentView(R.layout.weather_activity);
+            TextView textView = (TextView) findViewById(R.id.mLongitudeText);
+            //textView.setText("text you want to display");
+            textView.setText(String.valueOf(mLastLocation.getLatitude()));
+            //mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // We are not connected anymore!
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // We tried to connect but failed!
+
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
 }
